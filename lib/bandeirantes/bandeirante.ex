38c3e21cfe52b598c99f1efce380e760b4ac14c1
alexbispo <alexbispo.artes@gmail.com>
2018-11-24 00:12:31 -0200

    Implements Bandeirante navigation.

--- lib/bandeirantes/bandeirante.ex
+++ lib/bandeirantes/bandeirante.ex
@@ -1,8 +1,8 @@
 defmodule Bandeirantes.Bandeirante do
   
-  alias Bandeirantes.{Bandeirante, Coordinate}
+  alias Bandeirantes.{Bandeirante, Coordinate, Compass}
 
-  defstruct [:nav_limit, :nav_start, :nav_direction] 
+  defstruct [:nav_limit, :position, :nav_direction] 
 
   @doc """
     ## Examples
@@ -13,13 +13,45 @@ defmodule Bandeirantes.Bandeirante do
       {:ok, 
         %Bandeirantes.Bandeirante{
           nav_limit: %Bandeirantes.Coordinate{x: 5, y: 5},
-          nav_start: %Bandeirantes.Coordinate{x: 1, y: 2},
+          position: %Bandeirantes.Coordinate{x: 1, y: 2},
           nav_direction: "N"
         }
       }
 
   """
-  def new(%Coordinate{} = nav_limit, %Coordinate{} = nav_start, nav_direction) do
-    {:ok, %Bandeirante{nav_limit: nav_limit, nav_start: nav_start, nav_direction: nav_direction}}
+  def new(%Coordinate{} = nav_limit, %Coordinate{} = position, nav_direction) do
+    {:ok, %Bandeirante{nav_limit: nav_limit, position: position, nav_direction: nav_direction}}
+  end
+
+  @doc """
+    ## Examples
+
+      iex> {:ok, limit_coordinate} = Bandeirantes.Coordinate.new({5,5})
+      iex> {:ok, initial_coordinate} = Bandeirantes.Coordinate.new({1,2})
+      iex> {:ok, bandeirante} = Bandeirantes.Bandeirante.new(limit_coordinate, initial_coordinate, "N")
+      iex> bandeirante |> 
+      ...> Bandeirantes.Bandeirante.nav("L") |> 
+      ...> Bandeirantes.Bandeirante.nav("M") |>
+      ...> Bandeirantes.Bandeirante.nav("L") |> 
+      ...> Bandeirantes.Bandeirante.nav("M") |>
+      ...> Bandeirantes.Bandeirante.nav("L") |> 
+      ...> Bandeirantes.Bandeirante.nav("M") |>
+      ...> Bandeirantes.Bandeirante.nav("L") |> 
+      ...> Bandeirantes.Bandeirante.nav("M") |>
+      ...> Bandeirantes.Bandeirante.nav("M") 
+      %Bandeirantes.Bandeirante{
+        nav_limit: %Bandeirantes.Coordinate{x: 5, y: 5},
+        position: %Bandeirantes.Coordinate{x: 1, y: 3},
+        nav_direction: "N"
+      }
+  """
+  def nav(%Bandeirante{} = bandeirante, instruction) when instruction in ["L", "R"] do
+    {:ok, direction} = bandeirante.nav_direction |> Compass.turn(instruction)
+    %{bandeirante | nav_direction: direction}
+  end
+
+  def nav(%Bandeirante{} = bandeirante, instruction) when instruction === "M" do
+    new_position = bandeirante.position |> Coordinate.move(bandeirante.nav_direction)
+    %{bandeirante | position: new_position}
   end
 end
